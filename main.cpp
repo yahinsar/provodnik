55c44d0f856be06368cee2263c82906e07fa7ddd
Shamil Yahin <yahinshamil02@gmail.com>
2024-03-18 19:03:28 +0400

    Add main files

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <QCoreApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "database_manager.h"
+#include "map_manager.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<MapManager>("MapManager", 1, 0, "MapManager");
+
+    DatabaseManager databaseManager;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("databaseManager", &databaseManager);
+
+    if (!databaseManager.openDatabase("E:/Qt_projects/provodnik/newInfo.sqlite")) {
+        qDebug() << "Failed to open database.";
+        return -1;
+    }
+
+    const QUrl url(QUrl::fromLocalFile("E:/Qt_projects/provodnik/main.qml"));
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ main.qml
@@ -0,0 +1,17 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+import QtQuick.Layouts 1.15
+
+ApplicationWindow {
+    visible: true
+    width: 500
+    height: 800
+
+    StackView {
+        id: stackView
+        anchors.fill: parent
+
+        // Создаем экземпляр CustomPage с использованием sourceComponent
+        initialItem: LoginRegistration {}
+    }
+}
